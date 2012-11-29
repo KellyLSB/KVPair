a8e83eb2e83b8aaaa40ec692de236fb7f362944b
Kelly Becker <kellylsbkr@gmail.com>
2012-11-29 12:38:34 -0800

    kvpair

--- lib/kvpair.rb
+++ lib/kvpair.rb
@@ -8,7 +8,9 @@ module KVPair
 
 	module ClassMethods
 		def kvpair(ns)
+			nsm = ns.to_s + '-'
 			nse = ns.to_s + '='
+			nsa = ns.to_s + '<<'
 			send(:include, Module.new {
 				send(:define_method, ns.to_sym) do |*args|
 
@@ -72,7 +74,58 @@ module KVPair
 					# Remove unwanted keys
 					Kvpair.delete_all(["`owner` = ? && `namespace` = ? && (`key` NOT IN (?) || `value` = '' || `value` IS NULL)", "#{self.class.name}:#{self.id}", ns, keys])
 
-					# Return the switch
+					# Return the new pair
+					return self.send(ns)
+				end
+
+				send(:define_method, nsa.to_sym) do |input|
+
+					# Remove unknown keys
+					input.delete_if{|k,v| v.nil?}
+
+					# Upsert the record
+					input.each do |key, val|
+						begin
+							Kvpair.where(
+								:owner => "#{self.class.name}:#{self.id}",
+								:namespace => "#{ns}",
+								:key => "#{key}"
+							).first_or_create(:value => "#{val}")
+						rescue
+							pair = Kvpair.where(
+								:owner => "#{self.class.name}:#{self.id}",
+								:namespace => "#{ns}",
+								:key => "#{key}"
+							).first
+
+							pair.update_attribute(:value, val) unless pair.nil?
+
+							if pair.nil?
+								Kvpair.create(
+									:owner => "#{self.class.name}:#{self.id}",
+									:namespace => "#{ns}",
+									:key => "#{key}",
+									:value => "#{val}"
+								)
+							end
+						end
+					end
+
+					# Return the new pair
+					return self.send(ns)
+				end
+
+				send(:define_method, nsm.to_sym) do |input|
+
+					# Convert to array
+					input = input.keys if input.is_a?(Hash)
+					input = [input] if input.is_a?(String)
+					input = [input] if input.is_a?(Symbol)
+
+					# Delete the keys
+					Kvpair.delete_all(["`owner` = ? && `namespace` = ? && (`key` IN (?) || `value` = '' || `value` IS NULL)", "#{self.class.name}:#{self.id}", ns, input])
+
+					# Return the new pair
 					return self.send(ns)
 				end
 			})
